0bd4db9c11eb0586cee9985f4e17f44a357185e0
PKS3201090459 <126985161+PKS3201090459@users.noreply.github.com>
2025-09-14 00:50:13 +0500

    Add files via upload

---
+++ Solana/defi-landing/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // сканирует твой src на классы
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};``
